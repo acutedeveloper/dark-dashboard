f47347359969ea379d40a11099e3821330e4d752
acutedeveloper <nutyga@gmail.com>
2018-03-21 23:17:02 +0000

    Finishing Circle Charts

--- assets/js/area-charts.js
+++ assets/js/area-charts.js
@@ -1,55 +1,64 @@
 // try and combine both jQuery and D3js
-var areaChartData = [11,12,8,12,23]
 
-width = parseInt(d3.select('.areachart__graph').style('width'), 10);
-height = 260;
+function drawAreaChart(color){
+    var areaChartData = [11,12,8,12,23]
 
-var areaChartCanvas = d3.selectAll(".areachart__graph")
-    .append("svg")
-    .attr("class", "areachart__svg")
-    .attr("height", height)
-    .attr("width", width);
+    var areaChartText = {
+        price: "£310,394,685",
+        campaigns: 12,
+        percentage: 50
+    }
+    width = parseInt(d3.select('.areachart__graph').style('width'), 10);
+    height = 260;
 
+    var areaChartCanvas = d3.selectAll(".areachart__graph--" + color)
+        .append("svg")
+        .attr("class", "areachart__svg")
+        .attr("height", height)
+        .attr("width", width);
 
 // Lets create our scale
-var yScale = d3.scaleLinear()
-    .domain([0,d3.max(areaChartData, function(d){
-        return d;
-    })])
-    .range([height, 0]);
+    var yScale = d3.scaleLinear()
+        .domain([0,d3.max(areaChartData, function(d){
+            return d;
+        })])
+        .range([height, 0]);
 
-var xScale = d3.scaleLinear()
-    .domain([0, 4])
-    .range([0, width]);
+    var xScale = d3.scaleLinear()
+        .domain([0, 4])
+        .range([0, width]);
 
 // No axis is required
 
 // Map the data to the area chart
-var areaChart = d3.area()
-    .x(function(d,i) { return xScale(i); })
-    .y1(function(d) { return yScale(d); })
-    .y0(height);
+    var areaChart = d3.area()
+        .x(function(d,i) { return xScale(i); })
+        .y1(function(d) { return yScale(d); })
+        .y0(height);
 
-// Think about a order for creating your charts
-// 1 Create the canvas
-// 2 Add the scale
-// 4 Lets add a line :)
-// 3 Map the data to svg elements
+    areaChartCanvas.append("path")
+        .data([areaChartData])
+        .attr("d", areaChart)
+        .attr("class", "areachart__fill--" + color);
 
+    areaChartCanvas.append("text")
+        .text(areaChartText.price)
+        .attr('x', width/2)
+        .attr('y', height - 40)
+        .attr('text-anchor', 'middle')
+        .attr('class', 'size-h1')
+        .attr('fill', 'rgba(255,255,255, 0.5)')
+        .attr('fill', '#FFF');
 
-// Second add the line to the charset
-// Because we have use the svg.line function to interpolate the data
-// We do no need to use the data() & enter() functions??
+    areaChartCanvas.append("text")
+        .text("Using " + areaChartText.campaigns + " Campaigns")
+        .attr('x', width/2)
+        .attr('y', height - 30)
+        .attr('text-anchor', 'middle')
+        .attr('alignment-baseline', 'hanging')
+        .attr('class', 'size-p')
+        .attr('fill', 'rgba(255,255,255, 0.5)');
+}
 
-areaChartCanvas.append("path")
-    .data([areaChartData])
-    .attr("d", areaChart)
-    .attr("fill", "red");
-
-areaChartCanvas.append("text")
-    .text("Using 13 Campaigns")
-    .attr('x', width/2)
-    .attr('y', height - 35)
-    .attr('text-anchor', 'middle')
-    .attr('class', 'size-p')
-    .attr('fill', '#FFF');
+drawAreaChart("red");
+drawAreaChart("blue");
--- assets/js/circular-charts.js
+++ assets/js/circular-charts.js
@@ -1,38 +1,40 @@
-// try and combine both jQuery and D3js
-var circleChartData = {
-    price: "£310,394,685",
-    campaigns: 12,
-    percentage: 50
-}
 
-width = parseInt(d3.select('.circularchart__graph').style('width'), 10);
-height = 250;
 
-var circleChartCanvas = d3.selectAll(".circularchart__graph")
-    .append("svg")
-    .attr("height", height)
-    .attr("width", width);
+function drawCircleChart(color) {
+    var circleChartData = {
+        price: "£310,394,685",
+        campaigns: 12,
+        percentage: 50
+    }
 
-circleChartCanvas.append("text")
-    .text(circleChartData.price)
-    .attr('x', width/2)
-    .attr('y', height/2 + 5)
-    .attr('text-anchor', 'middle')
-    .attr('alignment-baseline', 'hanging')
-    .attr('class', 'size-h4')
-    .attr('fill', 'rgba(255,255,255, 0.5)');
+    width = parseInt(d3.select('.circularchart__graph').style('width'), 10);
+    height = 250;
 
-circleChartCanvas.append("text")
-    .text("Using " + circleChartData.campaigns + " Campaigns")
-    .attr('x', width/2)
-    .attr('y', height/2 - 5)
-    .attr('text-anchor', 'middle')
-    .attr('alignment-baseline', 'middle')
-    .attr('class', 'size-p')
-    .attr('fill', '#FFF');
+    var circleChartCanvas = d3.selectAll(".circularchart__graph--" + color)
+        .append("svg")
+        .attr("height", height)
+        .attr("width", width);
 
-var radius = 100;
-var perimiter = Math.PI * 2;
+    circleChartCanvas.append("text")
+        .text(circleChartData.price)
+        .attr('x', width/2)
+        .attr('y', height/2 - 15)
+        .attr('text-anchor', 'middle')
+        .attr('alignment-baseline', 'middle')
+        .attr('class', 'size-h1')
+        .attr('fill', '#FFF');
+
+    circleChartCanvas.append("text")
+        .text("Using " + circleChartData.campaigns + " Campaigns")
+        .attr('x', width/2)
+        .attr('y', height/2 + 5)
+        .attr('text-anchor', 'middle')
+        .attr('alignment-baseline', 'hanging')
+        .attr('class', 'size-p')
+        .attr('fill', 'rgba(255,255,255, 0.5)');
+
+    var radius = 100;
+    var perimiter = Math.PI * 2;
 
     var donutRadius = function(){
         if(width > height){
@@ -42,15 +44,41 @@ var perimiter = Math.PI * 2;
         }
     }
 
-var arc = d3.arc()
-    .innerRadius(donutRadius() - 15)
-    .outerRadius(donutRadius() - 10)
-    .startAngle(0)
-    .endAngle(perimiter);
+    var arc = d3.arc()
+        .innerRadius(donutRadius() - 15)
+        .outerRadius(donutRadius() - 10)
+        .startAngle(0)
+        // We multiply the %amount by the perimiter
+        .endAngle(0.65 * perimiter);
 
-var group = circleChartCanvas.append('g')
-    .attr("transform", "translate("+ (width/2) +", "+ (height/2) +")");
+    var arcMarker = d3.arc()
+        .innerRadius(donutRadius() - 15)
+        .outerRadius(donutRadius() - 10)
+        .startAngle(0.645 * perimiter)
+        // We multiply the %amount by the perimiter
+        .endAngle(0.65 * perimiter);
 
-group.append("path")
-    .attr("d", arc)
-    .attr("fill", "#E12E00");
+    var arcBackground = d3.arc()
+        .innerRadius(donutRadius() - 15)
+        .outerRadius(donutRadius() - 10)
+        .startAngle(0)
+        .endAngle(1  * perimiter);
+
+    var group = circleChartCanvas.append('g')
+        .attr("transform", "translate("+ (width/2) +", "+ (height/2) +")");
+
+    group.append("path")
+        .attr("d", arcBackground)
+        .attr('fill', 'rgba(255,255,255, 0.10)');
+
+    group.append("path")
+        .attr("d", arc)
+        .attr("class", "areachart__fill--" + color);
+
+    group.append("path")
+        .attr("d", arcMarker)
+        .attr('fill', '#FFF');
+}
+
+drawCircleChart("red");
+drawCircleChart("blue");
