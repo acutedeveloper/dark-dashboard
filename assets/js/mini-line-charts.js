ea3487b2561b47aa34dc9070fcd37f7fad87ce48
Nigel Peters <nutyga@gmail.com>
2018-03-23 22:24:43 +0000

    Embedding mini line charts

--- assets/js/mini-line-charts.js
+++ assets/js/mini-line-charts.js
@@ -1,10 +1,10 @@
-var lineChartCanvas = d3.selectAll("ticker-block__line-chart")
+var lineChartCanvas = d3.selectAll(".ticker-block__line-chart")
 		.append("svg")
-		.attr("height", 100)
-		.attr("width", 200)
-		.attr("fill", "orange");
-		
-var lineData = []
+		.attr("height", 53)
+		.attr("width", "100%")
+		.style("background-color", "green");
+
+var lineData = [];
 
 // Domain Scale
 
