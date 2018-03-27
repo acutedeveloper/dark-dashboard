18671fdb897a5de9d722d7ca9f69a7205c6dd788
acutedeveloper <nutyga@gmail.com>
2018-03-27 08:59:31 +0100

    Adding mini graph

---
+++ assets/js/debounce.js
@@ -0,0 +1,22 @@
+debounce = function(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+function ready(fn){
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
--- assets/js/mini-line-charts.js
+++ assets/js/mini-line-charts.js
@@ -1,32 +1,69 @@
+var lineData = [23, 55, 41, 18, 39, 23, 53, 30, 67, 46, 50, 29, 36, 50, 23,];
+
 var height = 55,
-    width = 200;
+    width = parseInt(d3.select('.ticker-block__line-chart').style('width'), 10);
 
 var lineChartCanvas = d3.selectAll(".ticker-block__line-chart")
-		.append("svg")
-		.attr("height", height)
-		.attr("width", width);
-
-var lineData = [23,55,41,18,39,53,67,46,30,23,29,36,50];
+    .append("svg")
+    .attr("height", height)
+    .attr("width", width);
 
 // Domain Scale
 var xScale = d3.scaleLinear()
     .domain([0, lineData.length])
-    .range([0, width]);
+    .range([1, width]);
+
 
 var yScale = d3.scaleLinear()
-    .domain([0, d3.max(lineData, function(d){
-        return d;
+    .domain([0, d3.max(lineData, function (d) {
+        return d + 5;
     })])
-    .range([height+2, 0]);
+    .range([height + 2, 0]);
 
 // line mapping
 var lineFunction = d3.line()
-    .x(function(d, i){ return xScale(i); })
-    .y(function(d,i){ return yScale(d); });
+    .x(function (d, i) { return xScale(i); })
+    .y(function (d, i) { return yScale(d); });
 
 // applying line to canvas
-lineChartCanvas.append("path")
-    .attr("d", lineFunction(lineData))
+var path = lineChartCanvas.append("path")
+    .attr('d', lineFunction(lineData))
     .attr("fill", "none")
     .attr('stroke-width', 3)
-    .attr("stroke", "white");
+    .attr('stroke-linejoin', 'round')
+    .attr('stroke-linecap', 'round')
+    .attr("stroke", "#118DA9");
+
+//---------
+
+
+
+function miniLineChart() {
+
+
+    lineChartCanvas;
+
+    xScale.range([1, width]);
+
+    lineFunction
+        .x(function (d, i) { return xScale(i); })
+        .y(function (d, i) { return yScale(d); });
+
+    path.attr('d', lineFunction(lineData));
+
+}
+
+// miniLineChart();
+//
+//
+// // Create the listener function
+// var updateLayout = debounce(function(e) {
+//
+//     miniLineChart();
+//     // Does all the layout updating here
+//     console.log('The viewport width is: ' + window.innerWidth);
+//
+// }, 500); // Maximum run of once per 500 milliseconds
+//
+// //Add the event listener
+// window.addEventListener("resize", updateLayout, false);
